40ef6ae605efda4e7fc1fde6aab789f15afbefef
gibo <gilbert.andrianis@gmail.com>
2019-04-29 18:43:36 +0200

    components architecture implemented

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser())
 
 // render
 render(app, {
-  root: path.join(__dirname, 'views'),
+  root: path.join(__dirname, 'src/public/views'),
   layout : 'layout',
   viewExt: 'html',
   cache: false,
@@ -25,20 +25,32 @@ render(app, {
 
 // routes
 router.get('/', index)
-router.get('/userProfile', userProfile)
+router.get('/search', search)
+router.get('/cart', cart)
+router.get('/menu', menu)
 
 
-router.get('/add', showAdd)
-router.post('/add', add)
+// router.get('/add', showAdd)
+// router.post('/add', add)
 
 // index function
 async function index(ctx){
   await ctx.render('index')
 }
 
-// userProfile function
-async function userProfile(ctx){
-  await ctx.render('/user/profile')
+// search function
+async function search(ctx){
+  await ctx.render('search')
+}
+
+// cart function
+async function cart(ctx){
+  await ctx.render('cart')
+}
+
+// menu function
+async function menu(ctx){
+  await ctx.render('menu')
 }
 
 // showAdd fuction
@@ -55,9 +67,8 @@ async function add(ctx) {
 
 
 // router middleware
-app
-  .use(router.routes())
-  .use(router.allowedMethods())
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 
 app.listen(3000, console.log('server runnig'))
--- src/users/userController.js
+++ src/users/userController.js
@@ -1,4 +1,4 @@
-const User = require('../db/mongoose/userSchema')
+const User = require('./userSchema')
 
 
 // create
